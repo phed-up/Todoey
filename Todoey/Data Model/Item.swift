d6440bf4a513dda8f74313d4f5cbd04fe377fd80
Sandy Wong <sandywong@Sandys-MacBook-Pro-2.local>
2018-02-13 22:54:51 -0600

    Persisting data using Codable protocol and Plist

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by skwong on 2/11/18.
+//  Copyright © 2018 skwong. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+    
+}
